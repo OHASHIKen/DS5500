4d37e12043b340677ddb9ab55696ec04874f697b
Ken Ohashi <27489109+OHASHIKen@users.noreply.github.com>
2022-05-06 17:48:08 +0900

    script to check functions

---
+++ check_function.py
@@ -0,0 +1,16 @@
+
+import ds5500
+
+ds = ds5500.DS5500("192.168.8.11", "5198")
+ds.SetNumberOfPoints(100)
+data = ds.GetWaveForm()
+print(data)
+
+### trigger related
+ds.GetTriggerMode()
+ds.SetTriggerSource("CH1")
+ds.SetTriggerSlope("POS")
+ds.SetTriggerLevel(0.1)
+
+ds.GetTriggerFrequency()
+
